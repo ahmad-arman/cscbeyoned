b2e5ab0020b5a59e7e78a79f2aefe36223db28f0
ahmad-ahmad <aa0796780751@gmail.com>
2022-06-24 11:41:32 +0300

    edit server

--- src/db.js
+++ src/db.js
@@ -1,5 +1,7 @@
 const {Sequelize} = require('sequelize');
 require('dotenv').config()
+const config = require(__dirname +"/../config/config.json")[env]
+
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
 module.exports = sequelize;
